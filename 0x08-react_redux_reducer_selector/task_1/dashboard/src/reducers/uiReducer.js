963f9b70aa430eb6f2b1e72583e426d6f2266357
Samuel Odhiambo Obunge <samuelobunge@gmail.com>
2024-08-22 09:24:56 +0300

    update

--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.js
@@ -1,44 +1,32 @@
-// task_0/dashboard/src/reducers/uiReducer.js
+// task_1/dashboard/src/reducers/uiReducer.js
 
+import { Map } from 'immutable';
 import { 
-    DISPLAY_NOTIFICATION_DRAWER, 
-    HIDE_NOTIFICATION_DRAWER, 
-    LOGIN, 
-    LOGOUT 
-  } from '../actions/uiActionTypes';
-  
-  const initialState = {
-    isNotificationDrawerVisible: false,
-    isUserLoggedIn: false,
-    user: {},
-  };
-  
-  const uiReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case DISPLAY_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: true,
-        };
-      case HIDE_NOTIFICATION_DRAWER:
-        return {
-          ...state,
-          isNotificationDrawerVisible: false,
-        };
-      case LOGIN:
-        return {
-          ...state,
-          isUserLoggedIn: true,
-        };
-      case LOGOUT:
-        return {
-          ...state,
-          isUserLoggedIn: false,
-        };
-      default:
-        return state;
-    }
-  };
-  
-  export default uiReducer;
-  
+  DISPLAY_NOTIFICATION_DRAWER, 
+  HIDE_NOTIFICATION_DRAWER, 
+  LOGIN, 
+  LOGOUT 
+} from '../actions/uiActionTypes';
+
+const initialState = Map({
+  isNotificationDrawerVisible: false,
+  isUserLoggedIn: false,
+  user: {},
+});
+
+const uiReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case DISPLAY_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', true);
+    case HIDE_NOTIFICATION_DRAWER:
+      return state.set('isNotificationDrawerVisible', false);
+    case LOGIN:
+      return state.set('isUserLoggedIn', true);
+    case LOGOUT:
+      return state.set('isUserLoggedIn', false);
+    default:
+      return state;
+  }
+};
+
+export default uiReducer;
--- 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/reducers/uiReducer.test.js
@@ -1,5 +1,6 @@
-// task_0/dashboard/src/reducers/uiReducer.test.js
+// task_1/dashboard/src/reducers/uiReducer.test.js
 
+import { Map } from 'immutable';
 import uiReducer from './uiReducer';
 import { 
   DISPLAY_NOTIFICATION_DRAWER, 
@@ -9,44 +10,44 @@ import {
 } from '../actions/uiActionTypes';
 
 describe('uiReducer', () => {
-  const initialState = {
+  const initialState = Map({
     isNotificationDrawerVisible: false,
     isUserLoggedIn: false,
     user: {},
-  };
+  });
 
   it('should return the initial state when no action is passed', () => {
-    expect(uiReducer(undefined, {})).toEqual(initialState);
+    expect(uiReducer(undefined, {}).toJS()).toEqual(initialState.toJS());
   });
 
   it('should return the initial state when SELECT_COURSE action is passed', () => {
-    expect(uiReducer(initialState, { type: 'SELECT_COURSE' })).toEqual(initialState);
+    expect(uiReducer(initialState, { type: 'SELECT_COURSE' }).toJS()).toEqual(initialState.toJS());
   });
 
   it('should change isNotificationDrawerVisible to true when DISPLAY_NOTIFICATION_DRAWER is passed', () => {
-    expect(uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER })).toEqual({
-      ...initialState,
+    expect(uiReducer(initialState, { type: DISPLAY_NOTIFICATION_DRAWER }).toJS()).toEqual({
+      ...initialState.toJS(),
       isNotificationDrawerVisible: true,
     });
   });
 
   it('should change isNotificationDrawerVisible to false when HIDE_NOTIFICATION_DRAWER is passed', () => {
-    expect(uiReducer(initialState, { type: HIDE_NOTIFICATION_DRAWER })).toEqual({
-      ...initialState,
+    expect(uiReducer(initialState, { type: HIDE_NOTIFICATION_DRAWER }).toJS()).toEqual({
+      ...initialState.toJS(),
       isNotificationDrawerVisible: false,
     });
   });
 
   it('should set isUserLoggedIn to true when LOGIN is passed', () => {
-    expect(uiReducer(initialState, { type: LOGIN })).toEqual({
-      ...initialState,
+    expect(uiReducer(initialState, { type: LOGIN }).toJS()).toEqual({
+      ...initialState.toJS(),
       isUserLoggedIn: true,
     });
   });
 
   it('should set isUserLoggedIn to false when LOGOUT is passed', () => {
-    expect(uiReducer(initialState, { type: LOGOUT })).toEqual({
-      ...initialState,
+    expect(uiReducer(initialState, { type: LOGOUT }).toJS()).toEqual({
+      ...initialState.toJS(),
       isUserLoggedIn: false,
     });
   });
